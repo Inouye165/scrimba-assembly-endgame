792b08f202bd2bc3a8a39c5cda750d3f93d2bf8c
inouye165 <inouye165@gmail.com>
2025-06-27 07:56:29 -0700

    feat: Added archery.js to hold words for an archery theme

---
+++ src/archery.js
@@ -0,0 +1,47 @@
+export const languages = [
+    {
+        name: "Quiver",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Arrow",
+        backgroundColor: "#328AF1",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Bowstring",
+        backgroundColor: "#F4EB13",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Nocking",
+        backgroundColor: "#2ED3E9",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Fletching",
+        backgroundColor: "#298EC6",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Target",
+        backgroundColor: "#599137",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Recurve",
+        backgroundColor: "#FFD742",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Compound",
+        backgroundColor: "#D02B2B",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Longbow",
+        backgroundColor: "#2D519F",
+        color: "#F9F4DA",
+    },
+]
